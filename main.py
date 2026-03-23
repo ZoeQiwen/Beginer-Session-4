7574cd361fb2ef0fbdcae705dfe917511b9ae00b
Zoe Qiwen <qiwenkolimntcode@gmail.com>
2026-03-23 12:27:21 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# import random
+# import modul_saya
+# print(modul_saya.nama_favorit)
+# acak = random.randint(7,9)
+# print(acak)
+# angka_acak_0_1 = random.random() * 10
+# print(angka_acak_0_1)
+# random_float = random.uniform(1,10)
+# print(random_float)
+import random
+asep = random.randint(0,1)
+if asep == 0:
+    print("kolimnt")
+if asep == 1 :
+    print("kolimnt code")
